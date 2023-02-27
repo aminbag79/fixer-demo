ef21f862366d9b69d1492b8c68608df4caa20a23
amin baghestani <bagamin97@gmail.com>
2023-02-27 18:51:10 +0330

    Initial commit

---
+++ say_hi/__init__.py
@@ -0,0 +1,3 @@
+from say_hi.handler import say_hi
+
+__all__=['say_hi']
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='fixer-demo',
+      packages=['say_hi'],
+      version='0.1',
+      license='MIT',
+      author='amin',
+      description='fixer demo say hi',
+      url='https://github.com/aminbag79/fixer-demo')
